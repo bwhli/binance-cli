66e43a6347be57477cba84c542b569132d878a9e
Brian Li <brian@brianli.com>
2022-07-16 22:02:28 -0400

    Add withdraw command.

---
+++ binance_cli/binance_client.py
@@ -0,0 +1,27 @@
+import typer
+from binance.client import Client
+from binance.enums import *
+from binance.exceptions import BinanceAPIException
+
+from binance_cli import ENV
+
+
+class BinanceClient:
+
+    BINANCE_API_KEY = ENV["API_KEY"]
+    BINANCE_SECRET_KEY = ENV["SECRET_KEY"]
+
+    def __init__(self) -> None:
+        self.client = Client(self.BINANCE_API_KEY, self.BINANCE_SECRET_KEY)
+
+    def withdraw(
+        self, asset: str, amount: float, address: str, address_tag: str = None
+    ):
+        try:
+            result = self.client.withdraw(
+                coin=asset, address=address, amount=amount, addressTag=address_tag
+            )
+            return result
+        except BinanceAPIException as e:
+            print(e)
+            raise typer.Exit()
--- binance_cli/main.py
+++ binance_cli/main.py
@@ -1,8 +1,17 @@
 import typer
 
+from binance_cli.binance_client import BinanceClient
+
 app = typer.Typer()
 
 
 @app.command()
 def debug():
     print(__name__)
+
+
+@app.command()
+def withdraw(asset: str, amount: float, address: str, address_tag: str = None):
+    binance_client = BinanceClient()
+    result = binance_client.withdraw(asset, amount, address, address_tag)
+    print(result)
